7b5f72a75da22e7d84875c9df56bb3a169595463
Bernardo-Lisboa <bernardo.al2006@aluno.ifsc.edu.br>
2026-08-04 19:53:41 -0300

    Initializing repo

---
+++ src/main/java/ifsc/pbe/PbeApplication.java
@@ -0,0 +1,14 @@
+package ifsc.pbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbeApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/ifsc/pbe/PbeApplicationTests.java
@@ -0,0 +1,13 @@
+package ifsc.pbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
