dfedbaebc856beb8fb2de8b927c238c8bb30be2e
Bernardo-Lisboa <bernardo.al2006@aluno.ifsc.edu.br>
2026-08-04 20:02:22 -0300

    feat: create a task on TaskService

--- src/main/java/ifsc/pbe/service/TaskService.java
+++ src/main/java/ifsc/pbe/service/TaskService.java
@@ -4,16 +4,22 @@ import ifsc.pbe.entity.Task;
 import ifsc.pbe.repository.TaskRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 @Service
 public class TaskService {
     TaskRepository taskRepository;
 
     public TaskService(){
     }
-    public void createTask(String name){
+    public void createTask(String name, String desc, LocalTime delTime, LocalDateTime dateTime,String anot){
         Task task = new Task();
-
-
+        task.setName(name);
+        task.setDesc(desc);
+        task.setDelTime(delTime);
+        task.setDateTime(dateTime);
+        task.setAnot(anot);
         taskRepository.save(task);
     }
 
