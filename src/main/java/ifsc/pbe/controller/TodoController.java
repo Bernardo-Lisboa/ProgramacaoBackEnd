af2d5bf82f33bb5a36c6a6a4c0312f512aced738
Bernardo Lisboa <bernardo.al2006@aluno.ifsc.edu.br>
2026-08-05 21:39:47 -0300

    fix: Controller signature fix

--- src/main/java/ifsc/pbe/controller/TodoController.java
+++ src/main/java/ifsc/pbe/controller/TodoController.java
@@ -2,6 +2,8 @@ package ifsc.pbe.controller;
 
 import ifsc.pbe.entity.Annotation;
 import ifsc.pbe.entity.Task;
+import ifsc.pbe.entity.TaskList;
+import ifsc.pbe.service.TaskService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,51 +13,59 @@ import java.util.List;
 @RequestMapping("/todo/v1")
 public class TodoController {
 
+    private final TaskService taskService;
+
+    public TodoController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
     // Create a list
     @PostMapping("/lists/{list}")
-    public ResponseEntity<Void> createList(@PathVariable("list") String list) {
-        return null;
+    public ResponseEntity<TaskList> createList(@PathVariable("list") String list) {
+        TaskList created = taskService.createTaskList(list);
+        return ResponseEntity.ok(created);
     }
 
     // All list names
     @GetMapping("/lists")
     public List<String> getListsNames() {
-        return null;
+        return taskService.getListNames();
     }
 
     // Task per List, ordered by deadline
     @GetMapping("/lists/{list}/tasks")
     public List<Task> getListTasks(@PathVariable("list") String list) {
-        return null;
+        return taskService.getListTasks(list);
     }
 
     // An especific task
     @GetMapping("/lists/{list}/tasks/{task}")
     public Task getListTask(@PathVariable("list") String list, @PathVariable("task") String task) {
-        return null;
+        return taskService.getTask(list, task);
     }
 
     // Add a task to a list
     @PostMapping("/lists/{list}/tasks")
     public Task addTask(@PathVariable("list") String list, @RequestBody Task task) {
-        return null;
+        return taskService.addTask(list, task);
     }
 
     // Add annotation to a task
     @PostMapping("/lists/{list}/tasks/{task}/annotations")
     public Annotation addAnnotation(@PathVariable("list") String list, @PathVariable("task") String task, @RequestBody Annotation annotation) {
-        return null;
+        return taskService.addAnnotation(list, task, annotation);
     }
 
     // Move a task to a list
     @PostMapping("/lists/{list}/tasks/{task}/move/{toList}")
     public Task moveTask(@PathVariable("list") String list, @PathVariable("task") String task, @PathVariable("toList") String toList) {
-        return null;
+        return taskService.moveTask(list, task, toList);
     }
 
     // Remove a task
     @DeleteMapping("/lists/{list}/tasks/{task}")
     public ResponseEntity<Void> removeTask(@PathVariable("list") String list, @PathVariable("task") String task) {
-        return null;
+        taskService.removeTask(list, task);
+        return ResponseEntity.noContent().build();
     }
 }
