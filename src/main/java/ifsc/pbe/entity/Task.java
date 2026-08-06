46da1ffd8d45df652105c73f987e3375940f630a
Bernardo Lisboa <bernardo.al2006@aluno.ifsc.edu.br>
2026-08-05 21:10:25 -0300

    fix: Entidade Task se encaixar nos conformes

--- src/main/java/ifsc/pbe/entity/Task.java
+++ src/main/java/ifsc/pbe/entity/Task.java
@@ -1,30 +1,33 @@
 package ifsc.pbe.entity;
 
-
 import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.util.List;
 
 @Data
 @Entity
 @Table(name = "tbl_task")
 public class Task {
+
     @Id
-    @Column(name="task_name")
+    @Column(name = "task_name")
     private String name;
 
-    @Column(name="task_desc")
+    @Column(name = "task_desc")
     private String desc;
 
-    @Column(name="task_delTime")
-    private LocalTime delTime;
+    @Column(name = "task_delTime")
+    private LocalDateTime delTime;
 
-    @Column(name="task_dateTime")
+    @Column(name = "task_dateTime")
     private LocalDateTime dateTime;
 
-    @Column(name="task_anot")
-    private String anot;
+    @ManyToOne
+    @JoinColumn(name = "list_name")
+    private TaskList list;
 
+    @OneToMany(mappedBy = "task", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Annotation> annotations;
 }
